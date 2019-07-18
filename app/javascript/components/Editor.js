a69329d0480c0e34ef16c10485eab89f6e61481f
adudley78 <dudleyadam@gmail.com>
2019-07-18 15:02:25 -0400

    Add PropsRoute

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import PropsRoute from './PropsRoute';
+import Spark from './Spark';
 import Header from './Header';
 import SparkList from './SparkList';
 
@@ -25,13 +28,26 @@ class Editor extends React.Component {
         const { sparks } = this.state;
         if (sparks === null) return null;
 
+        const { match } = this.props;
+        const sparkId = match.params.id;
+        const spark = sparks.find(e => e.id === Number(sparkId));
+
         return (
             <div>
                 <Header />
                 <SparkList sparks={sparks} />
+                <PropsRoute path="/sparks/:id" component={Spark} spark={spark} />
             </div>
         );
     }
 }
+
+Spark.propTypes = {
+    match: PropTypes.shape(),
+};
+
+Spark.defaultProps = {
+    match: undefined,
+};
 
 export default Editor;
