67e447dc94a3c2324e2a8ce6833b70c39e8e5311
adudley78 <dudleyadam@gmail.com>
2019-07-18 10:43:17 -0400

    Build out articles controller and install webpacker

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,36 @@
+class Api::ArticlesController < ApplicationController
+    respond_to :json
+
+    def index
+        respond_with Article.order(title: :DESC)
+    end
+
+    def show
+        respond_with Article.find(params[:id])
+    end
+
+    def create
+        respond_with :api, Article.create(article_params)
+    end
+
+    def destroy
+        respond_with Article.destroy(params[:id])
+    end
+
+    def update
+        article = Article.find(params['id'])
+        article.update(article_params)
+        respond_with Article, json: article
+    end
+
+    private
+
+    def article_params
+        param.require(:article).permit(
+            :title,
+            :url
+        )
+    end
+
+end
+    
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+    # Ensure that if no CSRF token is provided, Rails will respond with an empty session
+    protect_from_forgery with: :null_session
 end
